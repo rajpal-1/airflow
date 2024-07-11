f16b1de8432f29601ea03e16efc971de5d9c9085
sai-ganesh-03 <sandursaiganesh@gmail.com>
2024-07-11 11:19:50 +0530

    Replaced Bootstrap-typeahead with jquery-ui for typeahead functionality for searching other timezones, present at the navbar.

--- airflow/www/webpack.config.js
+++ airflow/www/webpack.config.js
@@ -234,6 +234,14 @@ const config = {
           flatten: true,
         },
         {
+          from: "node_modules/jquery-ui/dist/jquery-ui.min.js",
+          flatten: true,
+        },
+        {
+          from: "node_modules/jquery-ui/dist/themes/base/jquery-ui.min.css",
+          flatten: true,
+        },
+        {
           from: "node_modules/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js",
           flatten: true,
         },
